72d2b1447d0fe03cfbf3a26cd634aab63e7ce387
nobu <hiroki-pon@outlook.jp>
2023-02-09 14:20:11 +0900

    ログイン時のパスを作成

--- lib/utils/path.dart
+++ lib/utils/path.dart
@@ -1,9 +1,24 @@
 import 'package:go_router/go_router.dart';
+import 'package:twitter_clone/view/sign_in_up/screens/auth_code_screen.dart';
+import 'package:twitter_clone/view/sign_in_up/screens/customize_screen.dart';
+import 'package:twitter_clone/view/sign_in_up/screens/privacy_screen.dart';
+import 'package:twitter_clone/view/sign_in_up/screens/register_screen.dart';
 
-
-final router = GoRouter(
-  initialLocation: "/",
-  routes: [
-    // GoRoute(path: "/top",pageBuilder: (context, state) => NoTransitionPage(child: )),
-  ]
-);
+final router = GoRouter(initialLocation: "/", routes: [
+  GoRoute(
+      path: "/register",
+      pageBuilder: (context, state) =>
+          const NoTransitionPage(child: RegisterScreen())),
+  GoRoute(
+      path: "/customize",
+      pageBuilder: (context, state) =>
+          const NoTransitionPage(child: CustomizeScreen())),
+  GoRoute(
+      path: "/privacy",
+      pageBuilder: (context, state) =>
+          const NoTransitionPage(child: PrivacyScreen())),
+  GoRoute(
+      path: "/auth-code",
+      pageBuilder: (context, state) =>
+          const NoTransitionPage(child: AuthCodeScreen())),
+]);
