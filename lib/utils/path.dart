97b314e0259217c436bcef33ef6b097867778897
nobu <hiroki-pon@outlook.jp>
2023-02-09 17:59:41 +0900

    NoTransitionPageからMaterialPageに変更

--- lib/utils/path.dart
+++ lib/utils/path.dart
@@ -1,24 +1,45 @@
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/auth_code_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/customize_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/privacy_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/register_screen.dart';
 
-final router = GoRouter(initialLocation: "/", routes: [
-  GoRoute(
+final router = GoRouter(
+  initialLocation: "/",
+  routes: [
+    GoRoute(
+      path: "/",
+      pageBuilder: (context, state) => const MaterialPage(
+        child: PrivacyScreen(),
+      ),
+    ),
+    GoRoute(
       path: "/register",
-      pageBuilder: (context, state) =>
-          const NoTransitionPage(child: RegisterScreen())),
-  GoRoute(
+      pageBuilder: (context, state) => const MaterialPage(
+        child: RegisterScreen(),
+      ),
+    ),
+    GoRoute(
       path: "/customize",
-      pageBuilder: (context, state) =>
-          const NoTransitionPage(child: CustomizeScreen())),
-  GoRoute(
+      pageBuilder: (context, state) => const MaterialPage(
+        child: CustomizeScreen(),
+      ),
+    ),
+    GoRoute(
       path: "/privacy",
-      pageBuilder: (context, state) =>
-          const NoTransitionPage(child: PrivacyScreen())),
-  GoRoute(
+      pageBuilder: (context, state) => const NoTransitionPage(
+        child: PrivacyScreen(),
+      ),
+    ),
+    GoRoute(
       path: "/auth-code",
-      pageBuilder: (context, state) =>
-          const NoTransitionPage(child: AuthCodeScreen())),
-]);
+      pageBuilder: (context, state) => const MaterialPage(
+        child: AuthCodeScreen(),
+      ),
+    ),
+  ],
+  errorPageBuilder: (context, state) => const MaterialPage(
+    child: RegisterScreen(),
+  ),
+);
