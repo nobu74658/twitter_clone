4a16e71d8d6486d8a9c8ddab4b22209cbe71b072
nobu <hiroki-pon@outlook.jp>
2023-02-09 16:18:22 +0900

    テキストスタイルを追加

---
+++ lib/utils/styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const titleBold = TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+const bodyBold = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
