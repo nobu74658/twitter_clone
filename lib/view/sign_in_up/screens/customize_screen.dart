c1acc355e92a516768cc345a936b4a3444d2f212
nobu <hiroki-pon@outlook.jp>
2023-02-09 14:20:29 +0900

    ファイルを作成

---
+++ lib/view/sign_in_up/screens/customize_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomizeScreen extends StatelessWidget {
+  const CustomizeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
