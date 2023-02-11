4b5c0faf1b18c453d285871e7ab6a8d5733eee4f
nobu <hiroki-pon@outlook.jp>
2023-02-11 12:34:41 +0900

    パスを追加

--- lib/utils/path.dart
+++ lib/utils/path.dart
@@ -5,44 +5,47 @@ import 'package:twitter_clone/view/sign_in_up/screens/customize_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/privacy_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/register_confirm_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/register_screen.dart';
+import 'package:twitter_clone/view/top/screens/top_screen.dart';
+
+const kInitialPath = "/";
+const kCheckInviteEmailPath = "/check-invite-email";
+const kRegisterPath = "/register";
+const kRegisterConfirmPath = "/register-confirm";
+const kCustomizePath = "/customize";
+const kTopPath = "/top";
+const kPath = "";
 
 final GoRouter router = GoRouter(
   initialLocation: "/",
   routes: [
     GoRoute(
-      path: "/",
-      pageBuilder: (context, state) => const MaterialPage(
-        child: PrivacyScreen(),
-      ),
-    ),
-    GoRoute(
-      path: "/register",
+      path: kInitialPath,
       pageBuilder: (context, state) => const MaterialPage(
         child: RegisterScreen(),
       ),
     ),
     GoRoute(
-      path: "/register-confirm",
+      path: kTopPath,
+      pageBuilder: (context, state) => const MaterialPage(
+        child: TopScreen(),
+      ),
+    ),
+    GoRoute(
+      path: kRegisterPath,
+      pageBuilder: (context, state) => const MaterialPage(
+        child: RegisterScreen(),
+      ),
+    ),
+    GoRoute(
+      path: kRegisterConfirmPath,
       pageBuilder: (context, state) => const MaterialPage(
         child: RegisterConfirmScreen(),
       ),
     ),
     GoRoute(
-      path: "/customize",
+      path: kCustomizePath,
       pageBuilder: (context, state) => const MaterialPage(
         child: CustomizeScreen(),
-      ),
-    ),
-    GoRoute(
-      path: "/privacy",
-      pageBuilder: (context, state) => const NoTransitionPage(
-        child: PrivacyScreen(),
-      ),
-    ),
-    GoRoute(
-      path: "/auth-code",
-      pageBuilder: (context, state) => const MaterialPage(
-        child: AuthCodeScreen(),
       ),
     ),
   ],
