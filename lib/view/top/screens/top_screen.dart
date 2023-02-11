80e36d7ee005b8cb6da6f9c404600c8f171b4dd9
nobu <hiroki-pon@outlook.jp>
2023-02-11 14:43:23 +0900

    テスト用にサインアウトボタンと戻るボタンを追加

--- lib/view/top/screens/top_screen.dart
+++ lib/view/top/screens/top_screen.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/view/common/primary_app_bar.dart';
+import 'package:go_router/go_router.dart';
+import 'package:twitter_clone/utils/path.dart';
+import 'package:twitter_clone/view/common/components/primary_app_bar.dart';
 
 class TopScreen extends StatelessWidget {
   const TopScreen({super.key});
@@ -10,11 +11,20 @@ class TopScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PrimaryAppBar(appBar: AppBar()),
-      body: ElevatedButton(
-        onPressed: () {
-          FirebaseAuth.instance.signOut();
-        },
-        child: const Text("サインアウト"),
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: const Text("サインアウト"),
+          ),
+          const SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: () => context.go(kInitialPath),
+            child: const Text("戻る"),
+          ),
+        ],
       ),
     );
   }
