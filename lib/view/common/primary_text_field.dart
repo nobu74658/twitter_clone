bb8416de3fb7093a6795238db72a17acb9b07f9b
nobu <hiroki-pon@outlook.jp>
2023-02-11 11:22:31 +0900

    statelessに変更

--- lib/view/common/primary_text_field.dart
+++ lib/view/common/primary_text_field.dart
@@ -1,48 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:twitter_clone/view_model/login_view_model.dart';
 
-class PrimaryTextField extends StatefulWidget {
-  const PrimaryTextField({super.key, this.hintText, this.controller});
+class PrimaryTextField extends StatelessWidget {
+  const PrimaryTextField({
+    super.key,
+    this.hintText,
+    this.controller,
+    this.readOnly = false,
+    this.isEdit = false,
+    this.suffixIcon,
+  });
 
   final String? hintText;
   final TextEditingController? controller;
+  final bool readOnly;
+  final bool isEdit;
+  final Widget? suffixIcon;
 
-  @override
-  State<PrimaryTextField> createState() => _PrimaryTextFieldState();
-}
-
-bool isEdit = false;
-
-class _PrimaryTextFieldState extends State<PrimaryTextField> {
   @override
   Widget build(BuildContext context) {
+    final loginViewModel = context.read<LoginViewModel>();
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        isEdit ? Text(widget.hintText ?? "") : Text(""),
+        isEdit ? Text(hintText ?? "") : const Text(""),
         SizedBox(
           height: 36,
           child: TextFormField(
+            controller: controller,
+            readOnly: readOnly,
             style: const TextStyle(color: Colors.lightBlue),
             decoration: InputDecoration(
-              suffixIcon: Image.asset(
-                "assets/images/verified.png",
-                scale: 20,
-                color: Colors.green,
-              ),
+              suffixIcon: suffixIcon,
               focusedBorder: const UnderlineInputBorder(
                   borderSide: BorderSide(
                 color: Colors.black12,
               )),
-              hintText: widget.hintText,
+              hintText: hintText,
             ),
             onChanged: (change) {
-              if (change == "") {
-                isEdit = false;
-                setState(() {});
-              } else {
-                isEdit = true;
-                setState(() {});
-              }
+              loginViewModel.validation();
+              // if (change == "") {
+              //   // isEdit = false;
+              //   setState(() {
+              //     print(widget.controller?.text);
+              //   });
+              // } else {
+              //   isEdit = true;
+              //   setState(() {
+              //     print(widget.controller?.text);
+              // }
+              // );
+              // }
             },
           ),
         ),
