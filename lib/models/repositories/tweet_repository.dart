5ea35a76df6a90e1cddaea83b32c0b2b42b07097
nobu <hiroki-pon@outlook.jp>
2023-02-19 12:34:31 +0900

    tweet_repositoryを追加

---
+++ lib/models/repositories/tweet_repository.dart
@@ -0,0 +1,24 @@
+import 'package:twitter_clone/data_models/tweet.dart';
+import 'package:twitter_clone/models/db/database_manager.dart';
+import 'package:uuid/uuid.dart';
+
+class TweetRepository {
+  TweetRepository({required this.dbManager});
+
+  final DatabaseManager dbManager;
+
+  Future<void> postTweet({
+    required String userId,
+    required String desc,
+  }) async {
+    String tweetId = Uuid().v1();
+    Tweet tweet = Tweet(
+      tweetId: tweetId,
+      userId: userId,
+      favorite: 0,
+      desc: desc,
+      postDateTime: DateTime.now(),
+    );
+    await dbManager.insertTweet(tweet);
+  }
+}
