e21fb18fc10597767cc57b080bc1140a967d11fe
nobu <hiroki-pon@outlook.jp>
2023-02-24 11:05:19 +0900

    userName, userIcon, bioを追加

--- lib/data_models/tweet.dart
+++ lib/data_models/tweet.dart
@@ -1,6 +1,9 @@
 class Tweet {
   final String tweetId;
   final String userId;
+  final String userName;
+  final String? userIcon;
+  final String bio;
   final int favoriteNum;
   final String desc;
   final DateTime createdAt;
@@ -10,6 +13,9 @@ class Tweet {
   Tweet({
     required this.tweetId,
     required this.userId,
+    required this.userName,
+    this.userIcon,
+    required this.bio,
     required this.favoriteNum,
     required this.desc,
     required this.createdAt,
@@ -22,6 +28,9 @@ class Tweet {
           runtimeType == other.runtimeType &&
           tweetId == other.tweetId &&
           userId == other.userId &&
+          userName == other.userName &&
+          userIcon == other.userIcon &&
+          bio == other.bio &&
           favoriteNum == other.favoriteNum &&
           desc == other.desc &&
           createdAt == other.createdAt);
@@ -30,6 +39,9 @@ class Tweet {
   int get hashCode =>
       tweetId.hashCode ^
       userId.hashCode ^
+      userName.hashCode ^
+      userIcon.hashCode ^
+      bio.hashCode ^
       favoriteNum.hashCode ^
       desc.hashCode ^
       createdAt.hashCode;
@@ -39,6 +51,9 @@ class Tweet {
     return 'Tweet{' +
         ' tweetId: $tweetId,' +
         ' userId: $userId,' +
+        ' userName: $userName,' +
+        ' userIcon: $userIcon,' +
+        ' bio: $bio,' +
         ' favoriteNum: $favoriteNum,' +
         ' desc: $desc,' +
         ' createdAt: $createdAt,' +
@@ -46,15 +61,21 @@ class Tweet {
   }
 
   Tweet copyWith({
-    String? postId,
+    String? tweetId,
     String? userId,
+    String? userName,
+    String? userIcon,
+    String? bio,
     int? favoriteNum,
     String? desc,
     DateTime? createdAt,
   }) {
     return Tweet(
-      tweetId: postId ?? this.tweetId,
+      tweetId: tweetId ?? this.tweetId,
       userId: userId ?? this.userId,
+      userName: userName ?? this.userName,
+      userIcon: userIcon ?? this.userIcon,
+      bio: bio ?? this.bio,
       favoriteNum: favoriteNum ?? this.favoriteNum,
       desc: desc ?? this.desc,
       createdAt: createdAt ?? this.createdAt,
@@ -65,6 +86,9 @@ class Tweet {
     return {
       'tweetId': this.tweetId,
       'userId': this.userId,
+      'userName': this.userName,
+      'userIcon': this.userIcon,
+      'bio': this.bio,
       'favoriteNum': this.favoriteNum,
       'desc': this.desc,
       'createdAt': this.createdAt,
@@ -75,6 +99,9 @@ class Tweet {
     return Tweet(
       tweetId: map['tweetId'] as String,
       userId: map['userId'] as String,
+      userName: map['userName'] as String,
+      userIcon: map['userIcon'] as String?,
+      bio: map['bio'] as String,
       favoriteNum: map['favoriteNum'] as int,
       desc: map['desc'] as String,
       createdAt: map['createdAt'].toDate(),
