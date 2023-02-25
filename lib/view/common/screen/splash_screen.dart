8447b8909a2b37fba38114bdcf2716be4e4f865f
nobu <hiroki-pon@outlook.jp>
2023-02-25 15:59:15 +0900

    スプラッシュスクリーンは無し

--- lib/view/common/screen/splash_screen.dart
+++ lib/view/common/screen/splash_screen.dart
@@ -1,34 +1,45 @@
-import 'package:flutter/material.dart';
-import 'package:twitter_clone/utils/authentication.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:twitter_clone/utils/authentication.dart';
+// import 'package:twitter_clone/utils/path.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/logo.png",
-              color: Colors.white,
-            ),
-            const SizedBox(height: 40),
-            FutureBuilder(
-              future: Authentication.initializeFirebase(context: context),
-              builder: (context, snapshot) {
-                return const CircularProgressIndicator(
-                  color: Colors.white,
-                );
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.lightBlue,
+//       body: Center(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset(
+//               "assets/images/logo.png",
+//               color: Colors.white,
+//             ),
+//             const SizedBox(height: 40),
+//             FutureBuilder(
+//               future: initializeFirebase(context: context),
+//               builder: (context, snapshot) {
+//                 return const CircularProgressIndicator(
+//                   color: Colors.white,
+//                 );
+//               },
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<String> _initializeFirebase({
+//     required BuildContext context,
+//   }) async {
+//     User? user = FirebaseAuth.instance.currentUser;
+//     await Future.delayed(const Duration(seconds: 3));
+    
+//   }
+// }
