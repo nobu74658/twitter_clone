c557711d0c13def52f37f908a5944cae2c3447f1
nobu <hiroki-pon@outlook.jp>
2023-02-25 16:00:03 +0900

    スプラッシュスクリーンは無し

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,22 +1,22 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:twitter_clone/utils/path.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:twitter_clone/utils/path.dart';
 
-/// 参考
-/// https://blog.codemagic.io/firebase-authentication-google-sign-in-using-flutter-jpn/
+// /// 参考
+// /// https://blog.codemagic.io/firebase-authentication-google-sign-in-using-flutter-jpn/
 
-class Authentication {
-  static Future<void> initializeFirebase({
-    required BuildContext context,
-  }) async {
-    User? user = FirebaseAuth.instance.currentUser;
-    await Future.delayed(const Duration(seconds: 3));
+// class Authentication {
+//   static Future<void> initializeFirebase({
+//     required BuildContext context,
+//   }) async {
+//     User? user = FirebaseAuth.instance.currentUser;
+//     await Future.delayed(const Duration(seconds: 3));
 
-    if (user != null) {
-      context.go(kTopPath);
-    } else {
-      context.go(kLoginPath);
-    }
-  }
-}
+//     if (user != null) {
+//       context.go(kTopPath);
+//     } else {
+//       context.go(kLoginPath);
+//     }
+//   }
+// }
