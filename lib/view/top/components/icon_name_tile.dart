166094e9d92ae030cb45768c4f26d2ebd068ba7f
nobu <hiroki-pon@outlook.jp>
2023-03-02 15:17:07 +0900

    userDescのdata_modelをfreezedで作成

--- lib/view/top/components/icon_name_tile.dart
+++ lib/view/top/components/icon_name_tile.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/data_models/user_desc.dart';
+import 'package:twitter_clone/data_models/userDesc/user_desc.dart';
 
 class IconNameTile extends StatelessWidget {
   const IconNameTile({super.key, required this.user});
