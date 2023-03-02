8ad624ff2ccd8e5b072f24edf9e3be2d22553854
nobu <hiroki-pon@outlook.jp>
2023-03-02 11:25:10 +0900

    ShellRouterを使用

--- lib/utils/path.dart
+++ lib/utils/path.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:twitter_clone/view/common/screen/error_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/check_invite_email.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/customize_screen.dart';
@@ -9,18 +10,25 @@ import 'package:twitter_clone/view/sign_in_up/screens/pass_reset_confirm_screen.
 import 'package:twitter_clone/view/sign_in_up/screens/register_confirm_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/auth_screen.dart';
 import 'package:twitter_clone/view/sign_in_up/screens/reset_auth_screen.dart';
+import 'package:twitter_clone/view/top/bottom_navigation_bar.dart';
 import 'package:twitter_clone/view/top/pages/edit_profile_page.dart';
+import 'package:twitter_clone/view/top/pages/favorite_tweet_page.dart';
 import 'package:twitter_clone/view/top/pages/follow_follower_page.dart';
 import 'package:twitter_clone/view/top/pages/other_user_page.dart';
 import 'package:twitter_clone/view/top/pages/post_tweet_page.dart';
+import 'package:twitter_clone/view/top/pages/profile_page.dart';
+import 'package:twitter_clone/view/top/pages/time_line_page.dart';
 import 'package:twitter_clone/view/top/screens/top_screen.dart';
+import 'package:twitter_clone/view_model/favorite_view_model.dart';
+import 'package:twitter_clone/view_model/tweet_view_model.dart';
+
+import '../view_model/user_view_model.dart';
 
 const String kInitialPath = "/";
 const String kCheckInviteEmailPath = "/check-invite-email";
 const String kRegisterPath = "/register";
 const String kRegisterConfirmPath = "/register-confirm";
 const String kCustomizePath = "/customize";
-const String kTopPath = "/top";
 const String kLoginPath = "/login";
 const String kEditProfilePath = "/edit-profile";
 const String kPassResetConfirmPath = "/pass-reset-confirm";
@@ -31,11 +39,14 @@ const String kPostTweetPath = "/post-tweet";
 const String kFollowPath = "/follow";
 const String kFollowerPath = "/follower";
 const String kOtherUserPath = "/other-user";
-const String kPath = "";
+const String kTimeLinePath = "/time-line";
+const String kFavoriteTweetPath = "/favorite-tweet";
+const String kProfilePath = "/profile";
 
 final GoRouter router = GoRouter(
   initialLocation: kInitialPath,
-  redirect: (context, state) {
+  redirect: (context, state) async {
+    await _future(context);
     final firebaseUser = FirebaseAuth.instance.currentUser;
     String? path;
     if (firebaseUser == null) {
@@ -59,25 +70,25 @@ final GoRouter router = GoRouter(
     } else {
       switch (state.location) {
         case "":
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         case kInitialPath:
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         case kRegisterPath:
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         case kLoginPath:
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         case kRegisterConfirmPath:
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         case kCustomizePath:
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         case kCheckInviteEmailPath:
-          path = kTopPath;
+          path = kTimeLinePath;
           break;
         default:
       }
@@ -85,12 +96,6 @@ final GoRouter router = GoRouter(
     return path;
   },
   routes: [
-    GoRoute(
-      path: kTopPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: TopScreen(),
-      ),
-    ),
     GoRoute(
       path: kLoginPath,
       pageBuilder: (context, state) => const MaterialPage(
@@ -121,73 +126,105 @@ final GoRouter router = GoRouter(
         child: CheckInviteEmailScreen(),
       ),
     ),
-    GoRoute(
-      path: kEditProfilePath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: EditProfilePage(),
-      ),
-    ),
-    GoRoute(
-      path: kPassResetConfirmPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: PassResetConfirmScreen(),
-      ),
-    ),
-    GoRoute(
-      path: kPassResetAuthPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: ResetAuthScreen(
-          isPassReset: true,
-        ),
-      ),
-    ),
-    GoRoute(
-      path: kEmailResetConfirmPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: EmailResetConfirmScreen(),
-      ),
-    ),
-    GoRoute(
-      path: kEmailResetAuthPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: ResetAuthScreen(isPassReset: false),
-      ),
-    ),
-    GoRoute(
-      path: kPostTweetPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: PostTweetPage(),
-      ),
-    ),
-    GoRoute(
-      path: kFollowPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: FollowFollowerPage(),
-      ),
-    ),
-    GoRoute(
-      path: kFollowerPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: FollowFollowerPage(isFollow: false),
-      ),
-    ),
-    GoRoute(
-      path: kOtherUserPath,
-      pageBuilder: (context, state) => const MaterialPage(
-        child: TopScreen(),
-      ),
-    ),
-    GoRoute(
-      path: "$kOtherUserPath/:user_id",
-      pageBuilder: (context, state) {
-        String userId = state.params['user_id'] ?? "";
-        return MaterialPage(
-          child: OtherUserPage(otherUserId: userId),
-        );
+    ShellRoute(
+      builder: (BuildContext context, GoRouterState state, child) {
+        return BottomNavigation(context: context, child: child);
       },
+      routes: [
+        GoRoute(
+          path: kTimeLinePath,
+          builder: (context, state) {
+            return TimeLinePage();
+          },
+        ),
+        GoRoute(
+          path: kFavoriteTweetPath,
+          builder: (context, state) => FavoriteTweetPage(),
+        ),
+        GoRoute(
+          path: kProfilePath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: ProfilePage(),
+          ),
+        ),
+        GoRoute(
+          path: kEditProfilePath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: EditProfilePage(),
+          ),
+        ),
+        GoRoute(
+          path: kPassResetConfirmPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: PassResetConfirmScreen(),
+          ),
+        ),
+        GoRoute(
+          path: kPassResetAuthPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: ResetAuthScreen(
+              isPassReset: true,
+            ),
+          ),
+        ),
+        GoRoute(
+          path: kEmailResetConfirmPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: EmailResetConfirmScreen(),
+          ),
+        ),
+        GoRoute(
+          path: kEmailResetAuthPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: ResetAuthScreen(isPassReset: false),
+          ),
+        ),
+        GoRoute(
+          path: kPostTweetPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: PostTweetPage(),
+          ),
+        ),
+        GoRoute(
+          path: kFollowPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: FollowFollowerPage(),
+          ),
+        ),
+        GoRoute(
+          path: kFollowerPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: FollowFollowerPage(isFollow: false),
+          ),
+        ),
+        GoRoute(
+          path: kOtherUserPath,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: TopScreen(),
+          ),
+        ),
+        GoRoute(
+          path: "$kOtherUserPath/:user_id",
+          pageBuilder: (context, state) {
+            String userId = state.params['user_id'] ?? "";
+            return MaterialPage(
+              child: OtherUserPage(otherUserId: userId),
+            );
+          },
+        ),
+      ],
     ),
   ],
   errorPageBuilder: (context, state) => const MaterialPage(
     child: ErrorScreen(),
   ),
 );
+
+Future<void> _future(BuildContext context) async {
+  final userViewModel = context.read<UserViewModel>();
+  final favoriteViewModel = context.read<FavoriteViewModel>();
+  final tweetViewModel = context.read<TweetViewModel>();
+  await userViewModel.getCurrentUser();
+  await tweetViewModel.getCurrentUserTweet();
+  await favoriteViewModel.getFavoriteTweets();
+}
